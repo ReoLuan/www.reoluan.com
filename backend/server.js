64fb50fd6718485697029bf4f2dd49a472f11855
Jesus Nunez <your.email@example.com>
2025-03-29 21:14:52 -0400

    Fix path for index.html

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname));
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 // Verify environment variables
